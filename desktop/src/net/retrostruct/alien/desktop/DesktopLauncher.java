1e50f058a38ac714e82d8f4354a43835b5a0aa08
simon@igefjord.se <simon@igefjord.se>
2016-04-18 18:04:28 +0200

    Fix DesktopLauncher

--- desktop/src/net/retrostruct/alien/desktop/DesktopLauncher.java
+++ desktop/src/net/retrostruct/alien/desktop/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 900;
 		config.height = 480;
-		Game.MOBILE = true;
+		Game.MOBILE = false;
 		new LwjglApplication(new Game(), config);
 	}
 }
